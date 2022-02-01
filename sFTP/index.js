3e2c205daffe99a5dcebb30ac27b29f5bb6bb2dc
Ivan Cubides <ivandario1004@hotmaiil.com>
2022-01-31 20:47:11 -0500

    Mejora

---
+++ sFTP/index.js
@@ -0,0 +1,133 @@
+/*var bizagiUtil = require('bz-util');
+var REQUIRED = bizagiUtil.REQUIRED;
+var ERROR = bizagiUtil.error;
+var RESPONSE = bizagiUtil.getResponse;*/
+var Client = require('ssh2-sftp-client');
+const input = require('./input.js');
+
+debugger;
+/**
+ * @author Bizagi
+ */ 
+
+ function formatError(err) {
+    var errorMessage = '';
+    var errorObject = {
+        error: '',
+        message: '',
+        status: 0
+    };
+
+    if (err.code === 3) {
+        errorMessage = 'Please check that you have write permissions on the specified path to be able to upload the file.';
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 400;
+    }
+    else if (err.code === 2) {
+        errorMessage = 'Please check that the path exists.';
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 400;
+    }
+    else if (err.code === 4) {
+        errorMessage = 'Please check that the specified path isn\'t a folder.';
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 400;
+    }
+    else if (err.level === 'client-timeout' || err.code === 'ECONNREFUSED' || err.code === 'ENOTFOUND') {
+        errorMessage = 'Please check that the host address and port you\'re trying to connect to are correct';
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 400;
+    }
+    else if (err.level === 'client-authentication') {
+        errorMessage = 'Wrong username and/or password.';
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 401;
+    }
+    else {
+        errorMessage = 'Unexpected error occurred. ' + err.stack;
+        errorObject.error = errorMessage;
+        errorObject.message = errorMessage;
+        errorObject.status = 400;
+    }
+
+    return errorObject;
+}
+
+function invoke(globals, actionName, data, authenticationType, LOG, callback) {
+    const HOST = globals.authdata.HOST;
+    const PORT = globals.authdata.PORT;
+    const USERNAME = globals.authdata.USERNAME;
+    const PASSWORD = globals.authdata.PASSWORD;
+
+    const remote_path = data.inputs.input.remote_path;
+    const file_data = data.inputs.input.data;
+    
+    //Required parameters validation
+    /*var validationError = bizagiUtil.validator.requiredParameters({
+        'remote_path': remote_path,
+        'data': file_data,
+        'HOST': HOST,
+        'PORT': PORT,
+        'USERNAME': USERNAME,
+        'PASSWORD': PASSWORD,
+        'callback': callback
+    });
+
+    if (validationError) {
+        LOG.error(['[SFTP.', actionName, '] Error: ', validationError]);
+        callback(validationError);
+        return;
+    }*/
+
+    var sftp = new Client();
+
+    sftp.on('error', function (err) {
+        LOG.error(['[SFTP.', actionName, '] Error: ', err]);
+    });
+
+    sftp.on('close', function (err) {
+        LOG.debug(['[SFTP.', actionName, '] ClosedWithError: ', err]);
+    });
+
+    var fileData = new Buffer(file_data, 'base64');
+    var options = {
+        flags: 'w',  // w - write and a - append
+        encoding: 'utf8', // use null for binary files
+        mode: 0o666, // mode to use for created file (rwx)
+        autoClose: true // automatically close the write stream when finished
+    }
+    sftp.connect({
+            host: HOST,
+            port: PORT,
+            username: USERNAME,
+            password: PASSWORD
+        })
+        .then(function () {
+            return sftp.put(fileData, remote_path, options);
+        })
+        .then(function (data) {
+            var output = {
+                success: true
+            };
+            var success = RESPONSE(output, null, 200);
+            callback(success);
+            sftp.end();
+            return;
+        }).catch(function (err) {
+            LOG.error(['[SFTP.', actionName, '] Error: ', err]);
+            var errorObject = formatError(err);
+            var error = RESPONSE(null, errorObject, errorObject.status, errorObject.message);
+            callback(error);
+            sftp.end();
+            return;
+        });
+}
+
+
+invoke(input.globals, null, input.requestBizagi, null, null, null);
+exports.invoke = invoke;
---
+++ sFTP/input.js
@@ -0,0 +1,22 @@
+let requestBizagi = {
+    "inputs": {
+      "input": {
+        "data": "UFJPX09iamV0b1JlbGFjaW9uYWRvX19jO1BST19JZEV4dGVybm9fX2M7UFJPX1RpcG9Eb2N1bWVudG9Db250YWN0b19fYztQUk9fRG9jdW1lbnRvQ29udGFjdG9fX2M7UFJPX0FzdW50b19fYztQUk9fRGV0YWxsZTFfX2M7UFJPX0RldGFsbGUyX19jO1BST19UaXBvRG9jdW1lbnRvVXN1YXJpb19fYztQUk9fRG9jdW1lbnRvVXN1YXJpb19fYztQUk9fQXNpZ25hY2lvbl9fYztQUk9fTm9tYnJlQ29sYV9fYztQUk9fRXN0YWRvX19jO1BST19GZWNoYUFzaWduYWNpb25fX2M7UFJPX0ZlY2hhVmVuY2ltaWVudG9fX2M7UFJPX1RpcG9UYXJlYV9fYztQUk9fUHJpb3JpZGFkX19jO1BST19Qcm9jZXNhZG9fX2M7IFJlc3VsdGFkb0dlc3Rpb247IEdlc3Rpb25FZmVjdGl2YTsgRmVjaGFGaW4NCkN1ZW50YTtDQ18yNDk2MjA4MTMzO0NDOzI0OTYyMDgxMzM7IkNvbnRpbmdlbmNpYS9QcmV2aXNpb25hbC9QcmV2aXNpb25hbCBub3RpZmljYWNpb24ganVyaWRpY28iOyIiO2FzZDs7OyA7IDtDb21wbGV0YWRhOzMxLzAxLzIwMjIgODoyNDowMSBwLiBtLjswMi8wMi8yMDIyIDg6MjQ6MDEgcC4gbS47IkNvbnRpbmdlbmNpYS9QcmV2aXNpb25hbC9QcmV2aXNpb25hbCBub3RpZmljYWNpb24ganVyaWRpY28iO01lZGlhO1NpbiBQcm9jZXNhcjtDb250YWN0byBlZmVjdGl2bztTaTszMS8wMS8yMDIyIDg6MjU6MzUgcC4gbS4NCg0K",
+        "remote_path": "/../../E:/IntegracionSFDC/Import/CARGAINFOSERVICIOS/QATAREAS/data/PlantillaSalesForce-2022_1720.csv"
+      }
+    }
+  };
+
+module.exports.requestBizagi = requestBizagi;
+  
+let globals={
+    "authdata": {
+        "HOST": "172.16.2.36",
+        "PORT": "22",
+        "USERNAME": "BZGCXN1",
+        "PASSWORD": "x4NV5Hf6KL*"
+    }
+
+};
+
+module.exports.globals = globals;
