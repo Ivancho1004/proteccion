e4d8c916ac01cd01198562b7ea7431d4622d70b7
Ivan Cubides <ivandario1004@hotmaiil.com>
2022-03-08 22:24:58 -0500

    Mejora

--- SolicitudActualizar/input.js
+++ SolicitudActualizar/input.js
@@ -4,11 +4,11 @@ let requestBizagi = {
        "actualizar_solicitud": {
          "comando": {
            "aplicacionEmisora": {
-             "fechaTransaccion": "2022-03-08T22:03:57",
+             "fechaTransaccion": "2022-03-08T22:23:20",
              "idTransaccionEmisora": "ea3694bd-8d4d-49df-a126-63658f353faf",
              "nombreAplicacionEmisora": "Bizagi"
            },
-           "idTrazabilidad": "24b53354-6aa0-4d5e-9c52-65ebe4ac8c8e",
+           "idTrazabilidad": "a6d4272e-ea22-490d-993c-7109497354be",
            "nombre": "actualizarestadosolicitudcommand.solicitar",
            "payload": {
              "estado": "3",
@@ -28,28 +28,19 @@ let requestBizagi = {
            "header": {
              "params": [
                {
+                 "enable": true,
                  "name": "x-api-key",
                  "value": "YZNtu2sJDf6A2nbwmL0A34AbZyKVm8NQ1CnWz9Tb"
                },
                {
+                 "enable": true,
                  "name": "Content-Type",
                  "value": "application/json"
                },
                {
+                 "enable": true,
                  "name": "Authorization",
-                 "value": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCIsImtpZCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCJ9.eyJhdWQiOiJodHRwczovL1Byb3RlY2Npb25EMlBydWViYXMub25taWNyb3NvZnQuY29tLzgzNmFmZTgwLWM3OWEtNGYyMS05NjMzLWMxZTBkMGMwNDAyMCIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzRmMTZhMTM5LTQ4ZTktNGY4ZS05N2ZhLTRmOWZiODYwZjg1ZS8iLCJpYXQiOjE2NDY3OTM5NzAsIm5iZiI6MTY0Njc5Mzk3MCwiZXhwIjoxNjQ2Nzk3ODcwLCJhaW8iOiJFMlpnWU9BdzFyeW1kdUw4cTAwLzVqejQvejIxRmdBPSIsImFwcGlkIjoiODAwYzE3NjItMGQzYS00OTkwLWE1ZWMtOWQxNGZhYzA4MGMxIiwiYXBwaWRhY3IiOiIxIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlLyIsIm9pZCI6IjMyNzkzZDdjLWEwNTgtNGQ5ZS1hOWFmLTU0OGJiNzI0OTc0MSIsInJoIjoiMC5BUjBBT2FFV1QtbElqay1YLWstZnVHRDRYcXhlbGlpZktLeEN0aDVvajBqejhCVWRBQUEuIiwicm9sZXMiOlsiY2xhdmVfZW1wcmVzYXJpYWwiXSwic3ViIjoiMzI3OTNkN2MtYTA1OC00ZDllLWE5YWYtNTQ4YmI3MjQ5NzQxIiwidGlkIjoiNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlIiwidXRpIjoiTWRFeGQ3dkVJVTIyWW5VYl9iRU5BQSIsInZlciI6IjEuMCJ9.bWCog1pf7M-AM6wBoQPjI3dUGA8xjg1U7dxATwI-Zx2XDkBNJpPFnBwjuuSzQk6bg3u8PyWSTXivmLuEH11WlxMZPmK2U-OGLsl0-EjrCShPH8hmDRcsktc7g55ld1Gxqo9O0ww-BF0inkQeFtOFmayMTSRiaIpJrKOxH6BEh_IukV7JxjhSSaIivq8bJ2iDzhwHrjrKCh4n0jIwdHC5eZrPlzCc485EAMJQkm7kIQaOaXxoZewM0xEW_6Z-6Z4whS3I9jVLj7F_FPpYgYtHoS1DAVZLasVQd_qCDWjok3oL4xvEGaHI1Sw_qshSP9HSKET6jKz-E3LMjOH4A_U_qQ"
-               },
-               {
-                 "name": "x-api-key",
-                 "value": "YZNtu2sJDf6A2nbwmL0A34AbZyKVm8NQ1CnWz9Tb"
-               },
-               {
-                 "name": "Content-Type",
-                 "value": "application/json"
-               },
-               {
-                 "name": "Authorization",
-                 "value": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCIsImtpZCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCJ9.eyJhdWQiOiJodHRwczovL1Byb3RlY2Npb25EMlBydWViYXMub25taWNyb3NvZnQuY29tLzgzNmFmZTgwLWM3OWEtNGYyMS05NjMzLWMxZTBkMGMwNDAyMCIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzRmMTZhMTM5LTQ4ZTktNGY4ZS05N2ZhLTRmOWZiODYwZjg1ZS8iLCJpYXQiOjE2NDY3OTQ3MzcsIm5iZiI6MTY0Njc5NDczNywiZXhwIjoxNjQ2Nzk4NjM3LCJhaW8iOiJFMlpnWUhoL1hKNXJJK2VsclhmbUNqNk5ZcnpOQndBPSIsImFwcGlkIjoiODAwYzE3NjItMGQzYS00OTkwLWE1ZWMtOWQxNGZhYzA4MGMxIiwiYXBwaWRhY3IiOiIxIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlLyIsIm9pZCI6IjMyNzkzZDdjLWEwNTgtNGQ5ZS1hOWFmLTU0OGJiNzI0OTc0MSIsInJoIjoiMC5BUjBBT2FFV1QtbElqay1YLWstZnVHRDRYcXhlbGlpZktLeEN0aDVvajBqejhCVWRBQUEuIiwicm9sZXMiOlsiY2xhdmVfZW1wcmVzYXJpYWwiXSwic3ViIjoiMzI3OTNkN2MtYTA1OC00ZDllLWE5YWYtNTQ4YmI3MjQ5NzQxIiwidGlkIjoiNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlIiwidXRpIjoiV29DTXBWMW5pVW03UFJVQlVod05BQSIsInZlciI6IjEuMCJ9.ljEKJTDg4aaFBgZ8U9c2ygA0L4whMSrE_MHEYsiHVdW1DXCPdZlv2sYQmPxfNkK37YAlG2XcgX5x4Ge1byTaP8RNX7fEVOuVZWj7d6K3SfNF5j_o_ZZBYlbUhnFusnFrco1BmrLSQiw6Wn59T5kB5_v9U68G75qJ5Gvn2osHlNYA9GbCsrfLH7oY3T-aSuMh1pepBwXjQf2OM8qJAYtJN2aUN9HiKoZ7LH6Mz23EXg5vXXF-99dEOeTI_t413-vLF0WUW0iNkILdAEq_zD1sa9HiC-IekvhijL3PWpJ_nSB6cibWGQ2UabrR5axK42-_CzfFJ5voVrCIGEA1SQfz5w"
+                 "value": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCIsImtpZCI6Ik1yNS1BVWliZkJpaTdOZDFqQmViYXhib1hXMCJ9.eyJhdWQiOiJodHRwczovL1Byb3RlY2Npb25EMlBydWViYXMub25taWNyb3NvZnQuY29tLzgzNmFmZTgwLWM3OWEtNGYyMS05NjMzLWMxZTBkMGMwNDAyMCIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzRmMTZhMTM5LTQ4ZTktNGY4ZS05N2ZhLTRmOWZiODYwZjg1ZS8iLCJpYXQiOjE2NDY3OTU2NTQsIm5iZiI6MTY0Njc5NTY1NCwiZXhwIjoxNjQ2Nzk5NTU0LCJhaW8iOiJFMlpnWUdpUW45aDBWbmwzcGMrODdHTXpIU3QxQUE9PSIsImFwcGlkIjoiODAwYzE3NjItMGQzYS00OTkwLWE1ZWMtOWQxNGZhYzA4MGMxIiwiYXBwaWRhY3IiOiIxIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlLyIsIm9pZCI6IjMyNzkzZDdjLWEwNTgtNGQ5ZS1hOWFmLTU0OGJiNzI0OTc0MSIsInJoIjoiMC5BUjBBT2FFV1QtbElqay1YLWstZnVHRDRYcXhlbGlpZktLeEN0aDVvajBqejhCVWRBQUEuIiwicm9sZXMiOlsiY2xhdmVfZW1wcmVzYXJpYWwiXSwic3ViIjoiMzI3OTNkN2MtYTA1OC00ZDllLWE5YWYtNTQ4YmI3MjQ5NzQxIiwidGlkIjoiNGYxNmExMzktNDhlOS00ZjhlLTk3ZmEtNGY5ZmI4NjBmODVlIiwidXRpIjoiUDhQTV83M1cwa2FFOTRqM3k0TU5BQSIsInZlciI6IjEuMCJ9.YSyShrT3vzs-UPKOiRsb4Cz0BRgwWPuAhj0Xd4j4eGzDnW3HHs1M8sougWXrNooxGsRl8YTYlw0ePOb5H_KEvwZ5rh8oj6Np5KGDopV6tHcTimEibbxFsGbzCa1mSP26DJOm_AqP4VKFgqLXlVcZxLiCQNdOtcd3h-Ftu0t0YUVBuAsycktyz2oaPJHVNM9Fc0lTvmH6V3zYMZ0YJ3sDfYPPLYPerFuC3X5rh3rnM6luvDErG6Mmq5QxAUUE3miPxIFOiN3tEjOJJKkMpSww1yTlNyri0mNUztFeJ0G54umb8HC_0bQVH3fgfh5aQ5BiTLoxM82aMbFyKMHmwIrBhg"
                }
              ]
            },
