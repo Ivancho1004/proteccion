ceaac69eebf83ea075226d603f5a41e3e740b09b
Ivan Cubides <ivandario1004@hotmaiil.com>
2022-03-15 11:11:09 -0500

    Mejora

--- CargaFile/index.js
+++ CargaFile/index.js
@@ -1,28 +1,81 @@
-var request = require('request');
-var fs = require('fs');
-debugger
-var nuevo = fs.createReadStream('/C:/Users/Ivan Cubides/Downloads/PDF Clave.pdf');
-debugger
-var options = {
-  'method': 'POST',
-  'url': 'https://apiprivada-qa.proteccion.com.co/sgi-qa/gei/nti/initializer/upload/v2',
-  'headers': {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiJodHRwczovL3Byb3RlY2Npb24ub25taWNyb3NvZnQuY29tL2dlaV9wcnVlYmFzIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZjQ3ZGNjZTQtMDJlNy00ZDg4LWE2NTktYTJkMGJiZTE3MGU3LyIsImlhdCI6MTY0NzM1MDE2MCwibmJmIjoxNjQ3MzUwMTYwLCJleHAiOjE2NDczNTQwNjAsImFpbyI6IkUyWUFndVc2bjdZOEVKT1hUbit1K1piUC93TUEiLCJhcHBpZCI6IjQzNjM3NDg2LWNkNjQtNDQ1YS1hZDlhLTQ2MjA2MTE3NWQwNCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2Y0N2RjY2U0LTAyZTctNGQ4OC1hNjU5LWEyZDBiYmUxNzBlNy8iLCJvaWQiOiJiNzQ2YmE4NC0yNTQ0LTQ4ZjctODdjNy0yNmIzOGZkNzQ5OTYiLCJyaCI6IjAuQVFvQTVNeDk5T2NDaUUybVdhTFF1LUZ3NTN4anc5RWVXUWRGZzAxamMtS1RsYzRLQUFBLiIsInJvbGVzIjpbImVudmlhcl9ub3RpZmljYWNpb25lcyJdLCJzdWIiOiJiNzQ2YmE4NC0yNTQ0LTQ4ZjctODdjNy0yNmIzOGZkNzQ5OTYiLCJ0aWQiOiJmNDdkY2NlNC0wMmU3LTRkODgtYTY1OS1hMmQwYmJlMTcwZTciLCJ1dGkiOiItcXdZSWFjWDZrMkhKZWRIUTV5TUFBIiwidmVyIjoiMS4wIn0.iTsDwQWTwjI-x4aYU4egvkZEAxyrXVzgGbO1XJnR3SURCZtiw6Vtrooj1mxr8AXzmz8-oT-MxRL5WbSmrMDKgfjmPwwNT2tRGtZzyipC9_ffYgewy_uoTbrMuPuuslBx-r-GDperg04B19i3B0ZCBomxWIJbQrXvbyH2cCY2w-Q6Yxvolb_tMmpjn_Y63T-VjxCdCmkpoaXcQrRfokV6uwIk_PaPFoaMF_85U4EzaIB5QCwLX0FGfs7lE142VUIDlT8zDiaJm3xZUtvAOQGB27iQHjPjkLEeERxuv8woLbjkl-P7CqZ70c0Tj6xfnqtA3s594EiVJti7yD0hIs_6Tg',
-    'x-api-key': 'YZNtu2sJDf6A2nbwmL0A34AbZyKVm8NQ1CnWz9Tb',
-    'ClienteDNI': 'CC-1234'
-  },
-  formData: {
-    'file': {
-      'value': fs.createReadStream('/C:/Users/icubides/Downloads/PDF Clave (6).pdf'),
-      'options': {
-        'filename': '/C:/Users/icubides/Downloads/PDF Clave (6).pdf',
-        'contentType': null
-      }
+/**
+ * @author Proteecion
+ */
+
+ const http = require('http');
+ const https = require('https');
+ var request = require('request');
+
+
+ //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+ debugger;
+ const input = require('./input');
+ var output = {};
+debugger;
+ function invoke(globals, actionName, data, authenticationType, LOG, callback) {
+    //var sUrlProxy = globals.systemproperties.UrlProxy; 
+    //var UrlLoadFile = globals.systemproperties.UrlLoadFile; 
+    var sUrlProxy = "http://10.66.3.149:8080"; 
+    var UrlLoadFile ="https://apiprivada-qa.proteccion.com.co/sgi-qa/gei/nti/initializer/upload/v2";
+    debugger;
+    try {
+        let promises = [];
+        debugger
+        paramsHeader = data.inputs.input.Header;
+        let requestPermissions = data.inputs.input.Body;    
+
+        let options = {
+            'method': 'POST',
+            'url': UrlLoadFile,
+            headers: paramsHeader,
+            proxy: sUrlProxy,
+            formData: {
+                'file': {
+                  'value': requestPermissions.file,
+                  'options': {
+                    'filename': requestPermissions.fileName,
+                    'contentType': null
+                  }
+                }
+              }
+        };
+
+       debugger
+        let client = ssl ? https : http;
+        request(options, function (error, response) {
+            var json = JSON.parse(response.body);
+            debugger
+            if (response.statusCode === 200) {               
+                //var success = RESPONSE(json, null, 200);
+                console.log(json);
+                //callback(success);
+             } else {
+                 var errorResponse = {
+                        "error": response.statusCode ,
+                        "message": json.Message,
+                        "status": json.StatusCode
+                    };
+                // error = RESPONSE(null, errorResponse, 500);
+                 console.log(errorResponse)
+                 //callback(error);
+             }
+        });
+    }catch (e) {
+        var errorResponse = {
+            "error": -500,
+            "message": "Error al ejecutar la peticion. Exception: "+e.message,
+            "status": -500
+        };
+        /*LOG.error(['[', actionName, '] Error: ', e.message]);
+        
+        var error = RESPONSE(null, errorResponse, 500 );*/
+        console.log(errorResponse);
+        //callback(error);
     }
-  }
-};
-debugger
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+}
+
+
+
+
+invoke(null, null, input.requestBizagi, null, null, null);
+exports.invoke = invoke;
